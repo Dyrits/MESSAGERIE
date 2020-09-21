fa6a2b49cd734164efea60b5262c13df11a1ae3e
dyrits <dyrits@outlook.fr>
2020-09-21 12:36:20 +0300

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MessagesComponent } from './messages/messages.component';
+import { UsersComponent } from './users/users.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MessagesComponent,
+    UsersComponent,
+    MessageBoxComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
